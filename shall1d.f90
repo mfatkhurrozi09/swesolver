94b83d1956fd796f9e9fc0e14e8bd349c373dd52
mfatkhurrozi09 <145209533+mfatkhurrozi09@users.noreply.github.com>
2025-07-28 06:46:29 +0700

    Update shall1d.f90

--- shall1d.f90
+++ shall1d.f90
@@ -1,59 +1,46 @@
 program sswe1d
 
   ! Simple Shallow water equation 1D
+  ! Fatkhurrozi - Ocean Engineering ITERA - Jul 2025
 	
   implicit none
 
   ! Parameters
-  integer, parameter :: nx = 201        ! number of spatial points
-  integer, parameter :: nt = 400        ! number of time steps
-  real(8), parameter :: dx = 1.0d0      ! spatial resolution
-  real(8), parameter :: dt = 0.1d0      ! time step
-  real(8), parameter :: g = 9.81d0      ! gravitational acceleration
-  real(8), parameter :: de = 10.0d0      ! constant water depth
+  integer, parameter :: nx = 201        
+  integer, parameter :: nt = 400        
+  real(8), parameter :: dx = 1.0d0      
+  real(8), parameter :: dt = 0.1d0      
+  real(8), parameter :: g = 9.81d0      
+  real(8), parameter :: de = 10.0d0      ! if proper
   
   real(8) :: detadta,dudta, detadtn,dudtn
-
-  ! Arrays to store the full time history
   real(8), dimension(nx, nt) :: eta, u
-
-  ! Temporary arrays for predictor step
   real(8), dimension(nx) :: etap, up, detadt, dudt, dd
-
-  ! Loop variables
   integer :: i, n
   real(8) :: x
-
-  ! File output
   integer :: outf
 
-  ! ------------------------------
-  ! Initialization at t = 0
-  ! ------------------------------
-  !eta(:,1) = 0.0d0
-  !u(:,1) = 0.0d0
-
+! -- initialization
   do i = 1, nx
      x = (i - 1) * dx
      eta(i,1)=0.
-	 u(i,1)=0.
-	 dd(i)=5-i*0.02
-	 !eta(i,1) = exp( -((x - 50.0d0)**2) / (2.0d0 * 25.0d0) )
+	u(i,1)=0.
+	dd(i)=5-i*0.02		! variying depth
   end do
 
-  ! ------------------------------
-  ! Time stepping loop
-  ! ------------------------------
+
+! -- time integration
  
 !boundary cond. 
  do n = 1, nt - 1
      eta(1,n)=0.5*sin(2*3.14/5*n*dt)
 	 !print*, eta(1,n)
 enddo 
-	 
-  do n = 1, nt-1
+print *, ' -- SSWE1D --'
+print *, 'Ocean Engineering ITERA - Jul 2025'
 
-	 ! Predictor step (forward difference)
+  do n = 1, nt-1
+  ! Predictor  
      do i = 1, nx
         detadt(i) 	= -dd(i)*(u(i+1,n) - u(i,n))/ dx
 		dudt(i) 	= -g*(eta(i+1,n) - eta(i,n))/dx
@@ -68,7 +55,7 @@ enddo
 	
 	 ! print*, up(i),etap(i)
 		
-     ! Corrector step (backward difference)
+! Corrector s
      do i=2,nx-1
 		detadtn		= -dd(i)*(up(i)-up(i-1))/dx
 		dudtn		= -g*(etap(i)-etap(i-1))/dx 
@@ -80,16 +67,17 @@ enddo
 		u(i,n+1)	= u(i,n)+dudta*dt
 	 enddo
 	 
-! 	Neumann boundary condition
+! Neumann bc
      u(nx,n+1) = u(nx - 1,n+1)
      eta(nx,n+1) = eta(nx - 1,n+1)
 
-
   end do
 
-  ! ------------------------------
-  ! Write u(x,t) to file
-  ! ------------------------------
+  print *, '.'
+  print *, '.'
+  
+! -- Write u(x,t) to file
+ 
   outf=10
   open(unit=outf, file="uoutput.txt", status="replace", action="write")
 
@@ -103,6 +91,6 @@ enddo
 
   close(outf)
 
-  print *, 'Finished. Output written to uoutput.txt.'
+  print *, ' -- Success! -- Printed to uoutput.txt.'
 
 end program sswe1d
